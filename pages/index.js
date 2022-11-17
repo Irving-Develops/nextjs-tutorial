5fe5f1e10d8b7ac700e171bde81206d586fddff9
Irving Arreola Palacios <irving.a789@gmail.com>
2022-11-16 19:32:29 -0600

    api routes

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import {getSortedPostsData} from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
